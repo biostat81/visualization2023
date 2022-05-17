52fae96f56304cebf9d77947abbfa6d5e5e25e6f
biostat81 <105686833+biostat81@users.noreply.github.com>
2022-05-17 11:06:08 +0900

    Add files via upload

---
+++ R_program/Ch1/Ch1_test.R
@@ -0,0 +1,38 @@
+setwd("C:\\Users\\SYP\\Dropbox\\KNOU_강의개편\\고급R활용\\R_exercise")
+
+
+2*3+1
+a<-2
+b<-3
+c<-1
+a*b+c
+d<-a*b+c
+d
+
+typeof(d)
+x<-"KNOU"
+x
+typeof(x)
+y<-TRUE
+y
+typeof(y)
+yy<-2>3
+yy
+typeof(yy)
+z<-as.factor(1)
+z
+typeof(z)
+is.factor(z)
+
+e<-c(1, 10, 100)
+e
+f<-1:5
+f
+g<-seq(1, 9, 2)
+g
+h<-rep(1, 5)
+h
+i<-c(e, f, g)
+i
+
+
